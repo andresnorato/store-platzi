3d4e34239dcd48b5d405d84be53a24413e125006
Carlos Andres Norato Gomez <carnorat@gmail.com>
2022-02-20 18:44:47 -0500

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,16 @@ export class AppComponent {
     age: 12,
     avatar:
       'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg',
-  };
+  }
+
+
+  toggleButton(){
+    this.buttondisabled = !this.buttondisabled;
+  }
+
+
+  increaseage(){
+    this.person.age += 1;
+  }
+
 }
