11a61891722298d2dfd6cfde98be194917f43a5e
Carlos Andres Norato Gomez <carnorat@gmail.com>
2022-05-24 12:09:03 -0500

    module categoryes and preloadAllModules

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from '../not-found/not-found.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -12,57 +11,60 @@ import { ProductDatailComponent } from './pages/product-datail/product-datail.co
 import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
-    {
-      path: '',
-      component: LayoutComponent,
-      children: [
-        {
-          path: '',
-          redirectTo: '/home',
-          pathMatch: 'full',
-        },
-        {
-          path: 'home',
-          component: HomeComponent,
-        },
-        {
-          path: 'product/:id',
-          component: ProductDatailComponent,
-        },
-        {
-          path: 'category/:id',
-          component: CategoryComponent,
-        },
-        {
-          path: 'notFound',
-          component: NotFoundComponent,
-        },
-        {
-          path: 'myCart',
-          component: MyCartComponent,
-        },
-        {
-          path: 'login',
-          component: LoginComponent,
-        },
-        {
-          path: 'register',
-          component: RegisterComponent,
-        },
-        {
-          path: 'profile',
-          component: ProfileComponent,
-        },
-        {
-          path: 'recovery',
-          component: RecoveryComponent,
-        },
-      ],
-    },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'category',
+        loadChildren: () =>
+          import('./pages/category/category.module').then(
+            (m) => m.CategoryModule
+          ),
+      },
+      {
+        path: 'product/:id',
+        component: ProductDatailComponent,
+      },
+      {
+        path: 'notFound',
+        component: NotFoundComponent,
+      },
+      {
+        path: 'myCart',
+        component: MyCartComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'recovery',
+        component: RecoveryComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class WebsiteRoutingModule { }
+export class WebsiteRoutingModule {}
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -4,7 +4,6 @@ import { WebsiteRoutingModule } from './website-routing.module';
 import { NavComponent } from './components/nav/nav.component';
 import { HighlightDirective } from '../shared/directives/highlight.directive';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -20,7 +19,6 @@ import { SwiperModule } from 'swiper/angular';
     NavComponent,
     HighlightDirective,
     HomeComponent,
-    CategoryComponent,
     MyCartComponent,
     LoginComponent,
     RegisterComponent,
